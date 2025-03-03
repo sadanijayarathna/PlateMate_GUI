0782df1cb4f0f11fcd0db54e43571dbffef06cc9
sadanijayarathna <sadanijayarathna@gmail.com>
2025-03-03 12:36:59 +0530

    food items

--- testproject/src/context/StoreContext.jsx
+++ testproject/src/context/StoreContext.jsx
@@ -1,12 +1,13 @@
 import { createContext } from "react";
-
+import { food_list } from "../assets/frontend_assets/assets";
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
 const contextValue = {
-
+      food_list
 }
+
 return(
     <StoreContext.Provider value={contextValue}>
       {props.children}
--- testproject/src/pages/Home/Home.jsx
+++ testproject/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
 //import Hero from '../../Components/Hero/Hero'
 
 const Home = () => {
@@ -12,6 +13,7 @@ const [category,setcategory] = useState("All");
     <div>
       <Header/>
      <ExploreMenu category={category} setCategory={setcategory}/>
+     <FoodDisplay category={category}/>
     </div>
   )
 }
